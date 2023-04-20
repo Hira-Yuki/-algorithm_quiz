8a4a26be5ad9643d557af2e4245de3e285891eb6
윤진영 <78160955+Hira-Yuki@users.noreply.github.com>
2023-04-20 22:22:32 +0900

    알고리즘 문제 풀이 업로드

---
+++ 03. 가운데 글자 가져오기.js
@@ -0,0 +1,30 @@
+// 가운데 글자 가져오기
+
+// 문제 설명
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+// 재한사항
+// s는 길이가 1 이상, 100이하인 스트링입니다.
+
+// 입출력 예
+// s	return
+// "abcde"	"c"
+// "qwer"	"we"
+
+function solution(s) {
+    // var answer = '';
+    // let mid =Math.floor(s.length / 2)
+
+    // if (s.length % 2 === 0) {
+    //     answer = s.substr(mid - 1, 2)
+    // } else {
+    //     answer = s[mid]
+    // }
+
+    // return answer;
+    let mid =Math.floor(s.length / 2)
+    return s.length % 2 === 0 ? answer = s.substr(mid - 1, 2) : answer = s[mid]
+}
+
+console.log(solution('abcde'))
+console.log(solution('qwer'))
